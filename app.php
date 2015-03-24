e1963707dc66587cd417056687a47dd40c5bd6b9
kcmallard <kylemallard@gmail.com>
2015-03-23 20:24:50 -0500

    Login Site in PHP and HTML

---
+++ app.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost";
+$username = "kcmallard";
+$password = "titans";
+$dbname = "users";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$sql = "INSERT INTO Users (username, password) VALUES ('$username', '$password')";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "You've been added as a new user!";
+}
+$conn->close();
+?>
